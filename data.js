200c45e6fe8c1cccb68050dd8bd08bf12530f27d
Daniel Santiago Cristancho Moreno <dcristancho@catastrobogota.gov.co>
2025-08-05 15:58:20 -0500

    Faltan ajustes de graficos

---
+++ data.js
@@ -0,0 +1,285 @@
+const propertyTypeData = {
+  2019: [
+    { value: 24575, name: 'Apartamento' },
+    { value: 8819, name: 'Casa' },
+    { value: 1104, name: 'Local' },
+    { value: 1355, name: 'Oficina' },
+    { value: 16043, name: 'Resto' }
+  ],
+  2020: [
+    { value: 24714, name: 'Apartamento' },
+    { value: 7509, name: 'Casa' },
+    { value: 969, name: 'Local' },
+    { value: 1207, name: 'Oficina' },
+    { value: 16733, name: 'Resto' }
+  ],
+  2021: [
+    { value: 38543, name: 'Apartamento' },
+    { value: 11497, name: 'Casa' },
+    { value: 1334, name: 'Local' },
+    { value: 1666, name: 'Oficina' },
+    { value: 25255, name: 'Resto' }
+  ],
+  2022: [
+    { value: 39275, name: 'Apartamento' },
+    { value: 11670, name: 'Casa' },
+    { value: 1323, name: 'Local' },
+    { value: 1654, name: 'Oficina' },
+    { value: 25138, name: 'Resto' }
+  ],
+  2023: [
+    { value: 26820, name: 'Apartamento' },
+    { value: 8709, name: 'Casa' },
+    { value: 1107, name: 'Local' },
+    { value: 1257, name: 'Oficina' },
+    { value: 16739, name: 'Resto' }
+  ],
+  2024: [
+    { value: 35055, name: 'Apartamento' },
+    { value: 9388, name: 'Casa' },
+    { value: 1301, name: 'Local' },
+    { value: 1360, name: 'Oficina' },
+    { value: 20227, name: 'Resto' }
+  ],
+  2025: [
+    { value: 16706, name: 'Apartamento' },
+    { value: 3825, name: 'Casa' },
+    { value: 470, name: 'Local' },
+    { value: 598, name: 'Oficina' },
+    { value: 9006, name: 'Resto' }
+  ],
+  semestre: {
+    "2019-1": [
+      { value: 10921, name: "Apartamento" },
+      { value: 3975, name: "Casa" },
+      { value: 466, name: "Local" },
+      { value: 544, name: "Oficina" },
+      { value: 6984, name: "Resto" }
+    ],
+    "2019-2": [
+      { value: 13654, name: "Apartamento" },
+      { value: 4844, name: "Casa" },
+      { value: 638, name: "Local" },
+      { value: 811, name: "Oficina" },
+      { value: 9059, name: "Resto" }
+    ],
+    "2020-1": [
+      { value: 8812, name: "Apartamento" },
+      { value: 2590, name: "Casa" },
+      { value: 324, name: "Local" },
+      { value: 476, name: "Oficina" },
+      { value: 5711, name: "Resto" }
+    ],
+    "2020-2": [
+      { value: 15902, name: "Apartamento" },
+      { value: 4919, name: "Casa" },
+      { value: 645, name: "Local" },
+      { value: 731, name: "Oficina" },
+      { value: 11022, name: "Resto" }
+    ],
+    "2021-1": [
+      { value: 17542, name: "Apartamento" },
+      { value: 5065, name: "Casa" },
+      { value: 546, name: "Local" },
+      { value: 712, name: "Oficina" },
+      { value: 11325, name: "Resto" }
+    ],
+    "2021-2": [
+      { value: 21001, name: "Apartamento" },
+      { value: 6432, name: "Casa" },
+      { value: 788, name: "Local" },
+      { value: 954, name: "Oficina" },
+      { value: 13930, name: "Resto" }
+    ],
+    "2022-1": [
+      { value: 18538, name: "Apartamento" },
+      { value: 5284, name: "Casa" },
+      { value: 609, name: "Local" },
+      { value: 676, name: "Oficina" },
+      { value: 11565, name: "Resto" }
+    ],
+    "2022-2": [
+      { value: 20737, name: "Apartamento" },
+      { value: 6386, name: "Casa" },
+      { value: 714, name: "Local" },
+      { value: 978, name: "Oficina" },
+      { value: 13573, name: "Resto" }
+    ],
+    "2023-1": [
+      { value: 11449, name: "Apartamento" },
+      { value: 3747, name: "Casa" },
+      { value: 477, name: "Local" },
+      { value: 476, name: "Oficina" },
+      { value: 6817, name: "Resto" }
+    ],
+    "2023-2": [
+      { value: 15371, name: "Apartamento" },
+      { value: 4962, name: "Casa" },
+      { value: 630, name: "Local" },
+      { value: 781, name: "Oficina" },
+      { value: 9922, name: "Resto" }
+    ],
+    "2024-1": [
+      { value: 15492, name: "Apartamento" },
+      { value: 4237, name: "Casa" },
+      { value: 554, name: "Local" },
+      { value: 550, name: "Oficina" },
+      { value: 8699, name: "Resto" }
+    ],
+    "2024-2": [
+      { value: 19563, name: "Apartamento" },
+      { value: 5151, name: "Casa" },
+      { value: 747, name: "Local" },
+      { value: 810, name: "Oficina" },
+      { value: 11528, name: "Resto" }
+    ],
+    "2025-1": [
+      { value: 16705, name: "Apartamento" },
+      { value: 3825, name: "Casa" },
+      { value: 470, name: "Local" },
+      { value: 598, name: "Oficina" },
+      { value: 9004, name: "Resto" }
+    ],
+    "2025-2": [
+      { value: 1, name: "Apartamento" },
+      { value: 0, name: "Casa" },
+      { value: 0, name: "Local" },
+      { value: 0, name: "Oficina" },
+      { value: 2, name: "Resto" }
+    ]
+  },
+  trimestre: {
+    "2019-1": [{ value: 5979, name: "Apartamento" }, { value: 2070, name: "Casa" }, { value: 240, name: "Local" }, { value: 256, name: "Oficina" }, { value: 3835, name: "Resto" }],
+    "2019-2": [{ value: 4942, name: "Apartamento" }, { value: 1905, name: "Casa" }, { value: 226, name: "Local" }, { value: 288, name: "Oficina" }, { value: 3149, name: "Resto" }],
+    "2019-3": [{ value: 7553, name: "Apartamento" }, { value: 2618, name: "Casa" }, { value: 325, name: "Local" }, { value: 422, name: "Oficina" }, { value: 4942, name: "Resto" }],
+    "2019-4": [{ value: 6101, name: "Apartamento" }, { value: 2226, name: "Casa" }, { value: 313, name: "Local" }, { value: 389, name: "Oficina" }, { value: 4117, name: "Resto" }],
+    "2020-1": [{ value: 5314, name: "Apartamento" }, { value: 1669, name: "Casa" }, { value: 249, name: "Local" }, { value: 331, name: "Oficina" }, { value: 3336, name: "Resto" }],
+    "2020-2": [{ value: 3498, name: "Apartamento" }, { value: 921, name: "Casa" }, { value: 75, name: "Local" }, { value: 145, name: "Oficina" }, { value: 2375, name: "Resto" }],
+    "2020-3": [{ value: 6393, name: "Apartamento" }, { value: 1911, name: "Casa" }, { value: 244, name: "Local" }, { value: 275, name: "Oficina" }, { value: 4414, name: "Resto" }],
+    "2020-4": [{ value: 9509, name: "Apartamento" }, { value: 3008, name: "Casa" }, { value: 401, name: "Local" }, { value: 456, name: "Oficina" }, { value: 6608, name: "Resto" }],
+    "2021-1": [{ value: 8217, name: "Apartamento" }, { value: 2327, name: "Casa" }, { value: 225, name: "Local" }, { value: 296, name: "Oficina" }, { value: 5081, name: "Resto" }],
+    "2021-2": [{ value: 9325, name: "Apartamento" }, { value: 2738, name: "Casa" }, { value: 321, name: "Local" }, { value: 416, name: "Oficina" }, { value: 6244, name: "Resto" }],
+    "2021-3": [{ value: 10017, name: "Apartamento" }, { value: 2964, name: "Casa" }, { value: 340, name: "Local" }, { value: 407, name: "Oficina" }, { value: 6401, name: "Resto" }],
+    "2021-4": [{ value: 10984, name: "Apartamento" }, { value: 3468, name: "Casa" }, { value: 448, name: "Local" }, { value: 547, name: "Oficina" }, { value: 7529, name: "Resto" }],
+    "2022-1": [{ value: 8574, name: "Apartamento" }, { value: 2437, name: "Casa" }, { value: 311, name: "Local" }, { value: 366, name: "Oficina" }, { value: 5365, name: "Resto" }],
+    "2022-2": [{ value: 9964, name: "Apartamento" }, { value: 2847, name: "Casa" }, { value: 298, name: "Local" }, { value: 310, name: "Oficina" }, { value: 6200, name: "Resto" }],
+    "2022-3": [{ value: 10048, name: "Apartamento" }, { value: 3097, name: "Casa" }, { value: 303, name: "Local" }, { value: 412, name: "Oficina" }, { value: 6372, name: "Resto" }],
+    "2022-4": [{ value: 10689, name: "Apartamento" }, { value: 3289, name: "Casa" }, { value: 411, name: "Local" }, { value: 566, name: "Oficina" }, { value: 7201, name: "Resto" }],
+    "2023-1": [{ value: 4278, name: "Apartamento" }, { value: 1340, name: "Casa" }, { value: 151, name: "Local" }, { value: 181, name: "Oficina" }, { value: 2461, name: "Resto" }],
+    "2023-2": [{ value: 7171, name: "Apartamento" }, { value: 2407, name: "Casa" }, { value: 326, name: "Local" }, { value: 295, name: "Oficina" }, { value: 4356, name: "Resto" }],
+    "2023-3": [{ value: 7337, name: "Apartamento" }, { value: 2381, name: "Casa" }, { value: 299, name: "Local" }, { value: 363, name: "Oficina" }, { value: 4627, name: "Resto" }],
+    "2023-4": [{ value: 8034, name: "Apartamento" }, { value: 2581, name: "Casa" }, { value: 331, name: "Local" }, { value: 418, name: "Oficina" }, { value: 5295, name: "Resto" }],
+    "2024-1": [{ value: 6599, name: "Apartamento" }, { value: 1872, name: "Casa" }, { value: 231, name: "Local" }, { value: 235, name: "Oficina" }, { value: 3627, name: "Resto" }],
+    "2024-2": [{ value: 8893, name: "Apartamento" }, { value: 2365, name: "Casa" }, { value: 323, name: "Local" }, { value: 315, name: "Oficina" }, { value: 5072, name: "Resto" }],
+    "2024-3": [{ value: 9074, name: "Apartamento" }, { value: 2357, name: "Casa" }, { value: 382, name: "Local" }, { value: 340, name: "Oficina" }, { value: 5102, name: "Resto" }],
+    "2024-4": [{ value: 10489, name: "Apartamento" }, { value: 2794, name: "Casa" }, { value: 365, name: "Local" }, { value: 470, name: "Oficina" }, { value: 6426, name: "Resto" }],
+    "2025-1": [{ value: 8773, name: "Apartamento" }, { value: 2119, name: "Casa" }, { value: 261, name: "Local" }, { value: 351, name: "Oficina" }, { value: 4763, name: "Resto" }],
+    "2025-2": [{ value: 7932, name: "Apartamento" }, { value: 1706, name: "Casa" }, { value: 209, name: "Local" }, { value: 247, name: "Oficina" }, { value: 4241, name: "Resto" }]
+  }
+};
+
+
+const datosChartLine = [
+  { tipo: "anio", periodo: "2019", total: 67697 },
+  { tipo: "anio", periodo: "2020", total: 67264 },
+  { tipo: "anio", periodo: "2021", total: 103197 },
+  { tipo: "anio", periodo: "2022", total: 103413 },
+  { tipo: "anio", periodo: "2023", total: 82958 },
+  { tipo: "anio", periodo: "2024", total: 90028 },
+  { tipo: "anio", periodo: "2025", total: 30605 },
+
+  { tipo: "semestre", periodo: "2019-1", total: 28952 },
+  { tipo: "semestre", periodo: "2019-2", total: 38745 },
+  { tipo: "semestre", periodo: "2020-1", total: 21744 },
+  { tipo: "semestre", periodo: "2020-2", total: 45520 },
+  { tipo: "semestre", periodo: "2021-1", total: 43876 },
+  { tipo: "semestre", periodo: "2021-2", total: 59321 },
+  { tipo: "semestre", periodo: "2022-1", total: 46650 },
+  { tipo: "semestre", periodo: "2022-2", total: 56763 },
+  { tipo: "semestre", periodo: "2023-1", total: 32523 },
+  { tipo: "semestre", periodo: "2023-2", total: 50435 },
+  { tipo: "semestre", periodo: "2024-1", total: 37446 },
+  { tipo: "semestre", periodo: "2024-2", total: 52582 },
+  { tipo: "semestre", periodo: "2025-1", total: 30602 },
+  { tipo: "semestre", periodo: "2025-2", total: 3 },
+
+  { tipo: "trimestre", periodo: "2019-1", total: 14230 },
+  { tipo: "trimestre", periodo: "2019-2", total: 14722 },
+  { tipo: "trimestre", periodo: "2019-3", total: 20537 },
+  { tipo: "trimestre", periodo: "2019-4", total: 18208 },
+  { tipo: "trimestre", periodo: "2020-1", total: 12648 },
+  { tipo: "trimestre", periodo: "2020-2", total: 9096 },
+  { tipo: "trimestre", periodo: "2020-3", total: 17220 },
+  { tipo: "trimestre", periodo: "2020-4", total: 28300 },
+  { tipo: "trimestre", periodo: "2021-1", total: 18973 },
+  { tipo: "trimestre", periodo: "2021-2", total: 24903 },
+  { tipo: "trimestre", periodo: "2021-3", total: 27254 },
+  { tipo: "trimestre", periodo: "2021-4", total: 32067 },
+  { tipo: "trimestre", periodo: "2022-1", total: 21300 },
+  { tipo: "trimestre", periodo: "2022-2", total: 25350 },
+  { tipo: "trimestre", periodo: "2022-3", total: 26754 },
+  { tipo: "trimestre", periodo: "2022-4", total: 30009 },
+  { tipo: "trimestre", periodo: "2023-1", total: 11180 },
+  { tipo: "trimestre", periodo: "2023-2", total: 21343 },
+  { tipo: "trimestre", periodo: "2023-3", total: 23871 },
+  { tipo: "trimestre", periodo: "2023-4", total: 26564 },
+  { tipo: "trimestre", periodo: "2024-1", total: 15103 },
+  { tipo: "trimestre", periodo: "2024-2", total: 22343 },
+  { tipo: "trimestre", periodo: "2024-3", total: 24438 },
+  { tipo: "trimestre", periodo: "2024-4", total: 28144 },
+  { tipo: "trimestre", periodo: "2025-1", total: 16267 },
+  { tipo: "trimestre", periodo: "2025-2", total: 14335 }
+];
+
+
+const datosPorUso = [
+  { tipo: "anio", periodo: "2019", no_residencial: 18502, residencial: 33394 },
+  { tipo: "anio", periodo: "2020", no_residencial: 18909, residencial: 32223 },
+  { tipo: "anio", periodo: "2021", no_residencial: 28255, residencial: 50040 },
+  { tipo: "anio", periodo: "2022", no_residencial: 28115, residencial: 50945 },
+  { tipo: "anio", periodo: "2023", no_residencial: 19103, residencial: 35529 },
+  { tipo: "anio", periodo: "2024", no_residencial: 22888, residencial: 44443 },
+  { tipo: "anio", periodo: "2025", no_residencial: 10074, residencial: 20531 },
+
+  { tipo: "semestre", periodo: "2019-1", no_residencial: 7994, residencial: 14896 },
+  { tipo: "semestre", periodo: "2019-2", no_residencial: 10508, residencial: 18498 },
+  { tipo: "semestre", periodo: "2020-1", no_residencial: 6511, residencial: 11402 },
+  { tipo: "semestre", periodo: "2020-2", no_residencial: 12398, residencial: 20821 },
+  { tipo: "semestre", periodo: "2021-1", no_residencial: 12583, residencial: 22607 },
+  { tipo: "semestre", periodo: "2021-2", no_residencial: 15672, residencial: 27433 },
+  { tipo: "semestre", periodo: "2022-1", no_residencial: 12850, residencial: 23822 },
+  { tipo: "semestre", periodo: "2022-2", no_residencial: 15265, residencial: 27123 },
+  { tipo: "semestre", periodo: "2023-1", no_residencial: 7770, residencial: 15196 },
+  { tipo: "semestre", periodo: "2023-2", no_residencial: 11333, residencial: 20333 },
+  { tipo: "semestre", periodo: "2024-1", no_residencial: 9803, residencial: 19729 },
+  { tipo: "semestre", periodo: "2024-2", no_residencial: 13085, residencial: 24714 },
+  { tipo: "semestre", periodo: "2025-1", no_residencial: 10072, residencial: 20530 },
+  { tipo: "semestre", periodo: "2025-2", no_residencial: 2, residencial: 1 },
+
+  { tipo: "trimestre", periodo: "2019-1", no_residencial: 4331, residencial: 8049 },
+  { tipo: "trimestre", periodo: "2019-2", no_residencial: 3663, residencial: 6847 },
+  { tipo: "trimestre", periodo: "2019-3", no_residencial: 5689, residencial: 10171 },
+  { tipo: "trimestre", periodo: "2019-4", no_residencial: 4819, residencial: 8327 },
+  { tipo: "trimestre", periodo: "2020-1", no_residencial: 3916, residencial: 6983 },
+  { tipo: "trimestre", periodo: "2020-2", no_residencial: 2595, residencial: 4419 },
+  { tipo: "trimestre", periodo: "2020-3", no_residencial: 4933, residencial: 8304 },
+  { tipo: "trimestre", periodo: "2020-4", no_residencial: 7465, residencial: 12517 },
+  { tipo: "trimestre", periodo: "2021-1", no_residencial: 5602, residencial: 10544 },
+  { tipo: "trimestre", periodo: "2021-2", no_residencial: 6981, residencial: 12063 },
+  { tipo: "trimestre", periodo: "2021-3", no_residencial: 7148, residencial: 12981 },
+  { tipo: "trimestre", periodo: "2021-4", no_residencial: 8524, residencial: 14452 },
+  { tipo: "trimestre", periodo: "2022-1", no_residencial: 6042, residencial: 11011 },
+  { tipo: "trimestre", periodo: "2022-2", no_residencial: 6808, residencial: 12811 },
+  { tipo: "trimestre", periodo: "2022-3", no_residencial: 7087, residencial: 13145 },
+  { tipo: "trimestre", periodo: "2022-4", no_residencial: 8178, residencial: 13978 },
+  { tipo: "trimestre", periodo: "2023-1", no_residencial: 2793, residencial: 5618 },
+  { tipo: "trimestre", periodo: "2023-2", no_residencial: 4977, residencial: 9578 },
+  { tipo: "trimestre", periodo: "2023-3", no_residencial: 5289, residencial: 9718 },
+  { tipo: "trimestre", periodo: "2023-4", no_residencial: 6044, residencial: 10615 },
+  { tipo: "trimestre", periodo: "2024-1", no_residencial: 4093, residencial: 8471 },
+  { tipo: "trimestre", periodo: "2024-2", no_residencial: 5710, residencial: 11258 },
+  { tipo: "trimestre", periodo: "2024-3", no_residencial: 5824, residencial: 11431 },
+  { tipo: "trimestre", periodo: "2024-4", no_residencial: 7261, residencial: 13283 },
+  { tipo: "trimestre", periodo: "2025-1", no_residencial: 5375, residencial: 10892 },
+  { tipo: "trimestre", periodo: "2025-2", no_residencial: 4697, residencial: 9638 }
+];
